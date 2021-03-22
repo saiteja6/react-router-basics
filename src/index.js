62ccd563b272d327c28a169232cc0ea331a083b3
saiteja6 <bendalamsaiteja6@gmail.com>
2021-03-22 18:29:05 +0530

    Add Home, About, Contact and App components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+const Home = () => {
+  return <h2>Home Page</h2>;
+};
+
+const About = () => {
+  return <h2>About Page</h2>;
+};
+
+const Contact = () => {
+  return <h2>Contact Page</h2>;
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Route path="/" component={Home} />
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
 <h1>Introduction to Routing</h1>,
